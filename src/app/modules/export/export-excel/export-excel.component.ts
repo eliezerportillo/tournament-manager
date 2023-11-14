dea6c2022ddfc0573935e719fefe59eb038cbbe9
Eliezer Portillo <eliezerportillo7@gmail.com>
2023-11-14 11:02:48 -0600

    added exported feature

--- src/app/modules/export/export-excel/export-excel.component.ts
+++ src/app/modules/export/export-excel/export-excel.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject } from '@angular/core';
+import { AngularFirestore, QueryFn } from '@angular/fire/compat/firestore';
+import { IMatch } from '@app-core/models/match';
+import { IPlayer } from '@app-core/models/player';
+import { ITeam } from '@app-core/models/team';
+import { AccountService } from '@app-core/services/account.service';
+import { firstValueFrom, map, tap } from 'rxjs';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-export-excel',
@@ -6,5 +13,135 @@ import { Component } from '@angular/core';
   styleUrls: ['./export-excel.component.scss']
 })
 export class ExportExcelComponent {
+  db = inject(AngularFirestore);
+  accountService = inject(AccountService);
+
+  collections: IExportedCollection[] = [
+    new TeamExportedCollection('Equipos', ref => ref.orderBy('Pts', 'desc')),
+    new PlayerExportedCollection('Jugadores', ref => ref.orderBy('equipo')),
+    new MatchExportedCollection('Partidos', ref => ref.orderBy('fecha').orderBy('hora')),
+  ];
+  exportingCompleted = false;
+  loading = false;
+
+  progressReporter = new EventEmitter<string>();
+
+  statusItems: string[] = [];
+
+  constructor() {
+    this.progressReporter.subscribe(status => this.statusItems.push(status));
+  }
+
+  async handleExport() {
+    this.loading = true;
+
+    await this.startExporting();
+
+    this.loading = false;
+    this.exportingCompleted = true;
+  }
+
+  async startExporting(): Promise<void> {
+
+    const workbook = XLSX.utils.book_new();
+
+    for (const collection of this.collections) {
+      let f_collection = this.db.collection<any>(collection.collectionName)
+      if (collection.query) {
+        f_collection = this.db.collection<any>(collection.collectionName, collection.query)
+      }
+      const data$ = f_collection
+        .snapshotChanges()
+        .pipe(
+          tap(items => {
+            this.progressReporter.emit(`Información de ${items.length} ${collection.collectionName}`)
+          }),
+          map(actions =>
+            actions.map(action => {
+              const data = action.payload.doc.data();
+              return collection.map(data);
+            })
+          )
+        );
+      const data = await firstValueFrom(data$);
+      const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data);
+      XLSX.utils.book_append_sheet(workbook, worksheet, collection.collectionName);
+
+    }
+    // Save the workbook to a file
+    this.progressReporter.emit(`Descargando archivo.`)
+    XLSX.writeFile(workbook, this.accountService.getTournamentName() + '.xlsx');
+  }
+}
+
+
+interface IExportedCollection {
+  query: QueryFn;
+  collectionName: string;
+  map(data: any): any[]
+}
+
+abstract class ExportedCollection<T> implements IExportedCollection {
+  constructor(public collectionName: string, public query: QueryFn) {
+  }
+
+  abstract map(data: T): any;
+}
+
+class TeamExportedCollection extends ExportedCollection<ITeam> {
+
+  override map(x: ITeam): any {
+    return {
+      nombre: x.nombre,
+      PJ: x.PJ,
+      G: x.G,
+      E: x.E,
+      P: x.P,
+      GF: x.GF,
+      GC: x.GC,
+      DG: x.DG,
+      Pts: x.Pts,
+      tendencia: x.tendencia
+    }
+  }
 
 }
+
+class PlayerExportedCollection extends ExportedCollection<IPlayer> {
+
+  override map(x: IPlayer): any {
+    return {
+      equipo: x.equipo,
+      jugador: x.jugador,
+      capitan: x.capitan ? 1 : 0,
+      portero: x.portero ? 1 : 0,
+      noBautizado: x.noBautizado,
+      amarillas: x.amarillas,
+      rojas: x.rojas,
+      goles: x.goles,
+      correo: x.correo
+    };
+  }
+
+}
+
+class MatchExportedCollection extends ExportedCollection<IMatch> {
+
+  override map(x: IMatch): any {
+    return {
+      etapa: x.etapa,
+      ordenEtapa: x.ordenEtapa,
+      esClasificacion: x.esClasificacion,
+      numero: x.numero,
+      local: x.local,
+      marcadorLocal: x.marcadorLocal,
+      marcadorVisita: x.marcadorVisita,
+      visita: x.visita,
+      fecha: x.fecha,
+      hora: x.hora,
+      campo: x.campo,
+    }
+  }
+
+}
+
--- src/app/modules/export/export.module.ts
+++ src/app/modules/export/export.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { ExportRoutingModule } from './export-routing.module';
 import { ExportExcelComponent } from './export-excel/export-excel.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MaterialModule } from '@app-material/material.module';
 
 
 @NgModule({
@@ -11,7 +14,8 @@ import { ExportExcelComponent } from './export-excel/export-excel.component';
   ],
   imports: [
     CommonModule,
-    ExportRoutingModule
+    ExportRoutingModule,
+    MaterialModule
   ]
 })
 export class ExportModule { }
