2256dc3d18f260694790d340571bf96af25a8bf4
Eliezer Portillo <eliezerportillo7@gmail.com>
2024-09-02 12:22:50 -0600

    feat: Share by image

--- src/app/modules/matches/lineups/lineup/lineup.component.ts
+++ src/app/modules/matches/lineups/lineup/lineup.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IMatch, Match } from '@app-core/models/match';
 import { AccountService } from '@app-core/services/account.service';
 import { MatchService } from '@app-core/services/match.service';
+
+import html2canvas from 'html2canvas';
+
 
 @Component({
   selector: 'app-lineup',
@@ -16,6 +19,9 @@ export class LineupComponent implements OnInit {
 
   match?: IMatch;
   accountService = inject(AccountService)
+
+  @ViewChild('lineupContainer', { static: false }) lineupContainer!: ElementRef;
+
 
   constructor(private route: ActivatedRoute, private matchService: MatchService) {
 
@@ -38,7 +44,7 @@ export class LineupComponent implements OnInit {
 
   }
 
-  
+
   isFinished(match: IMatch): boolean {
     return Match.isFinished(match.dateTime);
   }
@@ -57,6 +63,80 @@ export class LineupComponent implements OnInit {
     } catch (error) {
       console.error('Error sharing content:', error);
     }
+  }
+
+  async shareAsImage(match: IMatch) {
+    const element = this.lineupContainer.nativeElement; // Cambia esto al elemento específico que deseas capturar
+  
+    try {
+      const canvas = await html2canvas(element);
+      const dataUrl = canvas.toDataURL('image/png');
+      const blob = this.dataURLToBlob(dataUrl);
+  
+      if (blob) {
+        const title = this.getShareTitle(match);
+        const file = this.createFile(blob, title);
+        const filesArray = [file];
+  
+        if (this.canShareFiles(filesArray)) {
+          if (navigator.share) {
+            try {
+              await navigator.share({
+                title: title,
+                text: `${match.local} vs ${match.visita}. Campo ${match.campo}. ${match.dateTime}`,
+                files: filesArray,
+                url: window.location.href
+              });
+              console.log('Share was successful.');
+            } catch (error) {
+              console.error('Error sharing content:', error);
+              alert('Error sharing content. Please try again.');
+            }
+          } else {
+            alert('Web Share API is not supported in your browser.');
+          }
+        } else {
+          console.warn('Sharing files is not supported in your browser.');
+          alert('Sharing files is not supported in your browser.');
+        }
+      }
+    } catch (error) {
+      console.error('Screenshot capture failed', error);
+      alert('Screenshot capture failed. Please try again.');
+    }
+  }
+  
+  private dataURLToBlob(dataUrl: string): Blob {
+    const byteString = atob(dataUrl.split(',')[1]);
+    const mimeString = dataUrl.split(',')[0].split(':')[1].split(';')[0];
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ab], { type: mimeString });
+  }
+
+  private async canvasToBlob(canvas: HTMLCanvasElement): Promise<Blob | null> {
+    return new Promise<Blob | null>((resolve) => {
+      canvas.toBlob((b) => resolve(b));
+    });
+  }
+
+  private getShareTitle(match: IMatch): string {
+    return `${match.local} vs ${match.visita}. Campo ${match.campo}. ${match.dateTime}`;
+  }
+
+  private createFile(blob: Blob, title: string): File {
+    return new File([blob], title + '.png', { type: 'image/png' });
+  }
+
+  private canShareFiles(filesArray: File[]): boolean {
+    return navigator.canShare && navigator.canShare({ files: filesArray });
+  }
+
+  private async shareContent(title: string, url: string, files?: File[]) {
+    
   }
 
   private async loadMatch() {
